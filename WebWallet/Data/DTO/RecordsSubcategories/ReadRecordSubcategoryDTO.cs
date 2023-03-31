7f154b930370ef33791f6315993b40f9772f9ca4
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-03-31 20:45:58 -0300

    fix: id da categoria do readSubcategory

--- WebWallet/Data/DTO/RecordsSubcategories/ReadRecordSubcategoryDTO.cs
+++ WebWallet/Data/DTO/RecordsSubcategories/ReadRecordSubcategoryDTO.cs
@@ -7,6 +7,8 @@ namespace WebWallet.Data.DTO.RecordsSubcategories
         [Required]
         public int RecordSubcategoryID { get; set; }
         [Required]
+        public int RecordCategoryID { get; set; }
+        [Required]
         [StringLength(100)]
         public string Description { get; set; }
         public Decimal SubTotal { get; set; }
