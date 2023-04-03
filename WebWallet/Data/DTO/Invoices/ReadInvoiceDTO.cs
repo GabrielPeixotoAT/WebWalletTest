3a4af351eafaebb7d1ee669406025ce9960815bc
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-04-03 13:31:02 -0300

    feat: add classes DTO para Invoice

---
+++ WebWallet/Data/DTO/Invoices/ReadInvoiceDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebWallet.Models.Cards;
+
+namespace WebWallet.Data.DTO.Invoices
+{
+    public class ReadInvoiceDTO
+    {
+        [Required]
+        public int InvoiceID { get; set; }
+        [Required]
+        public DateTime DueDate { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public int InvoiceStatusID { get; set; }
+        [Required]
+        public int CardID { get; set; }
+    }
+}
