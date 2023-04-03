0027d56f1912860b19bf53f50d8c75200a9ad2aa
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-04-03 13:27:56 -0300

    feat: add classes DTO para Card

---
+++ WebWallet/Data/DTO/Card/CreateCardDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebWallet.Data.DTO.Card
+{
+    public class CreateCardDTO
+    {
+        [Required]
+        public string Number { get; set; }
+        [Required]
+        public int BankID { get; set; }
+        [Required]
+        public DateTime ExpiringDate { get; set; }
+        [Required]
+        public Decimal Limit { get; set; }
+        [Required]
+        public string InvoicesDueDate { get; set; }
+    }
+}
---
+++ WebWallet/Data/DTO/Card/UpdateCardDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebWallet.Data.DTO.Card
+{
+    public class UpdateCardDTO
+    {
+        [Required]
+        public int CardID { get; set; }
+        [Required]
+        public string Number { get; set; }
+        [Required]
+        public int BankID { get; set; }
+        [Required]
+        public DateTime ExpiringDate { get; set; }
+        [Required]
+        public Decimal Limit { get; set; }
+        [Required]
+        public string InvoicesDueDate { get; set; }
+    }
+}
