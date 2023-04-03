a25d0491e132c37e8ec98dd493fa9a661a4a5abc
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-04-02 23:03:46 -0300

    feat: add model de Banco

---
+++ WebWallet/Models/Cards/Bank.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebWallet.Models.Cards
+{
+    public class Bank
+    {
+        [Key]
+        [Required]
+        public int BankID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        public string UserID { get; set; }
+        public virtual IdentityUser User { get; set; }
+    }
+}
