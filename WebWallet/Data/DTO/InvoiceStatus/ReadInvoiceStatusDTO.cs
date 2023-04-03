08d303514dc84b60e21787342fd4fc1b671f773d
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-04-03 13:33:02 -0300

    feat: add classes DTO para InvoiceStatus

---
+++ WebWallet/Data/DTO/InvoiceStatus/ReadInvoiceStatusDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebWallet.Data.DTO.InvoiceStatus
+{
+    public class ReadInvoiceStatusDTO
+    {
+        [Required]
+        public int InvoiceStatusID { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
