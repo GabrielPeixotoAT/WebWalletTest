e7652a76644e13c5afcda05c0b057611821ebed2
Gabriel Peixoto <gabr.art.design@gmail.com>
2023-04-03 21:38:55 -0300

    feat: add migration CartoesECores

---
+++ WebWallet/Data/Migrations/20230404002728_CartaoesECores.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebWallet.Data.Migrations
+{
+    public partial class CartaoesECores : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ColorCode",
+                table: "Accounts",
+                type: "nvarchar(7)",
+                maxLength: 7,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Banks",
+                columns: table => new
+                {
+                    BankID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserID = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banks", x => x.BankID);
+                    table.ForeignKey(
+                        name: "FK_Banks_AspNetUsers_UserID",
+                        column: x => x.UserID,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InvoiceStatus",
+                columns: table => new
+                {
+                    InvoiceStatusID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InvoiceStatus", x => x.InvoiceStatusID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    CardID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BankID = table.Column<int>(type: "int", nullable: false),
+                    ExpiringDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Limit = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    InvoicesDueDate = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.CardID);
+                    table.ForeignKey(
+                        name: "FK_Cards_Banks_BankID",
+                        column: x => x.BankID,
+                        principalTable: "Banks",
+                        principalColumn: "BankID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                columns: table => new
+                {
+                    InvoiceID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    InvoiceStatusID = table.Column<int>(type: "int", nullable: false),
+                    CardID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.InvoiceID);
+                    table.ForeignKey(
+                        name: "FK_Invoices_Cards_CardID",
+                        column: x => x.CardID,
+                        principalTable: "Cards",
+                        principalColumn: "CardID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Invoices_InvoiceStatus_InvoiceStatusID",
+                        column: x => x.InvoiceStatusID,
+                        principalTable: "InvoiceStatus",
+                        principalColumn: "InvoiceStatusID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transaction",
+                columns: table => new
+                {
+                    TransactionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    InvoiceID = table.Column<int>(type: "int", nullable: false),
+                    RecordSubcategoryID = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transaction", x => x.TransactionID);
+                    table.ForeignKey(
+                        name: "FK_Transaction_Invoices_InvoiceID",
+                        column: x => x.InvoiceID,
+                        principalTable: "Invoices",
+                        principalColumn: "InvoiceID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Transaction_RecordSubcategories_RecordSubcategoryID",
+                        column: x => x.RecordSubcategoryID,
+                        principalTable: "RecordSubcategories",
+                        principalColumn: "RecordSubcategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "InvoiceStatus",
+                columns: new[] { "InvoiceStatusID", "Description" },
+                values: new object[,]
+                {
+                    { 1, "Aberta" },
+                    { 2, "Vencida" },
+                    { 3, "Parcialmente Paga" },
+                    { 4, "Paga" },
+                    { 5, "Cancelada" },
+                    { 6, "Atrasada" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Banks_UserID",
+                table: "Banks",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_BankID",
+                table: "Cards",
+                column: "BankID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_CardID",
+                table: "Invoices",
+                column: "CardID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_InvoiceStatusID",
+                table: "Invoices",
+                column: "InvoiceStatusID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaction_InvoiceID",
+                table: "Transaction",
+                column: "InvoiceID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaction_RecordSubcategoryID",
+                table: "Transaction",
+                column: "RecordSubcategoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transaction");
+
+            migrationBuilder.DropTable(
+                name: "Invoices");
+
+            migrationBuilder.DropTable(
+                name: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "InvoiceStatus");
+
+            migrationBuilder.DropTable(
+                name: "Banks");
+
+            migrationBuilder.DropColumn(
+                name: "ColorCode",
+                table: "Accounts");
+        }
+    }
+}
