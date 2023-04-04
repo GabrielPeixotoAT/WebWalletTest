5c267a2701b74ecdcdaace2ef1ace119b79c1e02
gabriel.peixoto <desenvolvimento14@resolveconsultoria.com>
2023-04-04 17:58:27 -0300

    feat: tela para gerenciamento de cartões

---
+++ WebWallet/Controllers/CardController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebWallet.Models.ViewModels;
+using WebWallet.Services.Auth.Interfaces;
+using WebWallet.Services.Cards.Interfaces;
+
+namespace WebWallet.Controllers
+{
+    public class CardController : Controller
+    {
+        ICardService cardService;
+        IUserService userService;
+
+        public CardController(
+            ICardService cardService,
+            IUserService userService)
+        {
+            this.cardService = cardService;
+            this.userService = userService;
+        }
+
+        public IActionResult Index()
+        {
+            CardViewModel model = new CardViewModel();
+
+            model.Cards = cardService.GetAll(userService.GetUserId());
+
+            return View(model);
+        }
+    }
+}
---
+++ WebWallet/Models/ViewModels/CardViewModel.cs
@@ -0,0 +1,9 @@
+﻿using WebWallet.Data.DTO.Card;
+
+namespace WebWallet.Models.ViewModels
+{
+    public class CardViewModel
+    {
+        public List<ReadCardDTO> Cards { get; set; }
+    }
+}
